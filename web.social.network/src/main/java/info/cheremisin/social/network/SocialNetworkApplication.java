61146ef4f2676c2f445deb7d9742af788ebc5e16
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-10-06 00:18:20 +0300

    Pack application to war for separate tomcat deployment. Closes #9

--- web.social.network/src/main/java/info/cheremisin/social/network/SocialNetworkApplication.java
+++ web.social.network/src/main/java/info/cheremisin/social/network/SocialNetworkApplication.java
@@ -2,9 +2,10 @@ package info.cheremisin.social.network;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SocialNetworkApplication {
+public class SocialNetworkApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
         SpringApplication.run(SocialNetworkApplication.class, args);
