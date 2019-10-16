689c02a8daf6cdfe4bd29a2e3fbb3dcc6773e02e
dmcheremisin <$isNumber1>
2019-10-16 19:51:31 +0300

    copy images from jar to folder

--- web.social.network/src/main/java/info/cheremisin/social/network/initializer/ImageInitializer.java
+++ web.social.network/src/main/java/info/cheremisin/social/network/initializer/ImageInitializer.java
@@ -1,14 +1,16 @@
 package info.cheremisin.social.network.initializer;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Profile;
 import org.springframework.context.event.ContextRefreshedEvent;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.core.io.Resource;
 import org.springframework.stereotype.Component;
 
 import java.io.File;
 import java.io.IOException;
+import java.io.InputStream;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
@@ -19,14 +21,27 @@ import java.nio.file.Paths;
 @Profile({"dev", "qa"})
 public class ImageInitializer implements ApplicationListener<ContextRefreshedEvent> {
 
+    private static final String PROFILE_IMAGES = "profileImages";
+
+    @Value("classpath:profileImages/*")
+    private Resource[] resources;
+
     @Override
     public void onApplicationEvent(ContextRefreshedEvent contextRefreshedEvent) {
-        System.out.println(">>>> Location: ");
-        Path path = Paths.get(".");
-        System.out.println(path.toAbsolutePath());
-        Resource resource = new ClassPathResource("profileImages/1.jpg");
         try {
-            File file = resource.getFile();
+            Path pathImages = Paths.get(".").resolve(PROFILE_IMAGES);
+            if(pathImages.toFile().exists()) {
+                Files.walk(pathImages)
+                        .map(Path::toFile)
+                        .forEach(File::delete);
+                Files.delete(pathImages);
+            }
+            Files.createDirectory(pathImages);
+
+            for (Resource resource : resources) {
+                InputStream inputStream = resource.getInputStream();
+                Files.copy(inputStream, pathImages.resolve(resource.getFilename()));
+            }
         } catch (IOException e) {
             e.printStackTrace();
         }
