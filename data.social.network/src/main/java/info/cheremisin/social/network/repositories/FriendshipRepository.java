2db2a6cce8573a0d8b20af2cdccf247f0e5effc4
Dmitrii Cheremisin <dmitry_cher@mail.ru>
2019-12-14 00:30:14 +0300

    Fixed Mysql issue

--- data.social.network/src/main/java/info/cheremisin/social/network/repositories/FriendshipRepository.java
+++ data.social.network/src/main/java/info/cheremisin/social/network/repositories/FriendshipRepository.java
@@ -5,6 +5,7 @@ import info.cheremisin.social.network.entities.User;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -14,24 +15,24 @@ public interface FriendshipRepository extends CrudRepository<Friendship, Long> {
 
     @Query("SELECT f from Friendship f WHERE (f.userSender.id = :userId OR f.userReceiver.id = :userId) " +
             "AND f.accepted = true")
-    List<Friendship> findAcceptedFriendshipUsers(Long userId);
+    List<Friendship> findAcceptedFriendshipUsers(@Param("userId") Long userId);
 
     @Modifying(clearAutomatically = true)
     @Query("DELETE FROM Friendship f WHERE (f.userSender = :user AND f.userReceiver = :friend) " +
             "OR (f.userSender = :friend AND f.userReceiver = :user)")
-    void deleteFriendRequests(User user, User friend);
+    void deleteFriendRequests(@Param("user") User user, @Param("friend") User friend);
 
     @Modifying(clearAutomatically = true)
     @Query(value = "insert into friendship (user_sender, user_receiver, accepted) VALUES (:user, :friend, true)",
             nativeQuery = true)
-    void addFriendship(User user, User friend);
+    void addFriendship(@Param("user") User user, @Param("friend") User friend);
 
     @Modifying(clearAutomatically = true)
     @Query(value = "insert into friendship (user_sender, user_receiver, accepted) VALUES (:user, :friend, false)",
             nativeQuery = true)
-    void addToFriends(User user, User friend);
+    void addToFriends(@Param("user") User user, @Param("friend") User friend);
 
     @Query("SELECT case when count(f)> 0 then true else false end FROM Friendship f WHERE (f.userSender = :user AND f.userReceiver = :friend) " +
             "OR (f.userSender = :friend AND f.userReceiver = :user)")
-    boolean checkFriendshipExists(User user, User friend);
+    boolean checkFriendshipExists(@Param("user") User user, @Param("friend") User friend);
 }
--- data.social.network/src/main/java/info/cheremisin/social/network/repositories/MessageRepository.java
+++ data.social.network/src/main/java/info/cheremisin/social/network/repositories/MessageRepository.java
@@ -3,6 +3,7 @@ package info.cheremisin.social.network.repositories;
 import info.cheremisin.social.network.entities.Message;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
@@ -10,11 +11,11 @@ public interface MessageRepository extends CrudRepository<Message, Long> {
 
     @Query(value="SELECT m FROM Message m WHERE m.id IN (SELECT MAX(id) FROM Message GROUP BY sender, receiver) " +
             " AND (m.sender.id = :id OR m.receiver.id = :id)")
-    List<Message> findAllRecentMessages(Long id);
+    List<Message> findAllRecentMessages(@Param("id") Long id);
 
     @Query(value="SELECT m FROM Message m WHERE (m.sender.id = :userId AND m.receiver.id = :companionId) OR " +
             "(m.sender.id = :companionId AND m.receiver.id = :userId) ORDER BY m.time")
-    List<Message> findConversation(Long userId, Long companionId);
+    List<Message> findConversation(@Param("userId") Long userId, @Param("companionId") Long companionId);
 
     Message findFirstBySenderIdOrReceiverIdOrderByIdDesc(Long userId, Long theSameUserId);
 }
