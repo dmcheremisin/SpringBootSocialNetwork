08753aefeb45151527052766d83da35b3fead5f1
dmcheremisin <$isNumber1>
2019-12-16 23:30:10 +0300

    Configured profiles for Heroku. #15

--- web.social.network/src/main/resources/data.sql
+++ web.social.network/src/main/resources/data.sql
@@ -1,26 +1,25 @@
-INSERT INTO role (name)
+INSERT INTO role (id, name)
 VALUES
-('ROLE_USER'),
-('ROLE_ADMIN');
+(1, 'ROLE_USER'),
+(2, 'ROLE_ADMIN');
 
-INSERT INTO user (username, password, first_name, last_name, date_of_birth, sex, phone, image)
+INSERT INTO user (id, username, password, first_name, last_name, date_of_birth, sex, phone, image)
 VALUES
-('tyrion@lannister.ru',   '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Tyrion',   'Lannister', '1987-09-28', 1, '+7 999 999 99 99', '1.jpg'),
-('cersei@lannister.ru',   '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Cersei',   'Lannister', '1978-09-28', 2, '+7 888 999 99 99', '2.png'),
-('jaime@lannister.ru',    '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Jaime',    'Lannister', '1975-09-28', 1, '+7 777 999 99 99', '3.jpeg'),
-('stark@north.no',        '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Ned',      'Stark',     '1965-11-15', 1, '+7 666 999 99 99', '4.jpg'),
-('astark@north.no',       '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Sansa',    'Stark',     '1992-04-26', 2, '+7 555 999 99 99', '5.jpg'),
-('aria.stark@north.no',   '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Aria',     'Stark',     '1996-12-15', 2, '+7 555 999 99 99', '6.jpg'),
-('john@north.no',         '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Jon',      'Snow',      '1988-10-11', 1, '+7 444 999 99 99', '7.jpg'),
-('dayneris@targarien.ta', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Daenerys', 'Targaryen', '1981-03-17', 2, '+7 333 999 99 99', '8.jpg'),
-('rob@north.no',          '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Rob',      'Stark',     '1988-10-13', 1, '+7 222 999 99 99', '9.jpg'),
-('tywin@lannister.la',    '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Tywin',    'Lannister', '1952-08-27', 1, '+7 111 999 99 99', '10.jpg'),
-('brienne@tarth.ta',      '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Brienne',  'Tarth',     '1967-02-19', 2, '+7 999 888 99 99', '11.jpg'),
-('lord@varys.va',         '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Lord',     'Varys',     '1954-12-01', 1, '+7 999 777 99 99', '12.jpeg');
-
+(1, 'tyrion@lannister.ru',   '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Tyrion',   'Lannister', '1987-09-28', 1, '+7 999 999 99 99', '1.jpg'),
+(2, 'cersei@lannister.ru',   '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Cersei',   'Lannister', '1978-09-28', 2, '+7 888 999 99 99', '2.png'),
+(3, 'jaime@lannister.ru',    '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Jaime',    'Lannister', '1975-09-28', 1, '+7 777 999 99 99', '3.jpeg'),
+(4, 'stark@north.no',        '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Ned',      'Stark',     '1965-11-15', 1, '+7 666 999 99 99', '4.jpg'),
+(5, 'astark@north.no',       '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Sansa',    'Stark',     '1992-04-26', 2, '+7 555 999 99 99', '5.jpg'),
+(6, 'aria.stark@north.no',   '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Aria',     'Stark',     '1996-12-15', 2, '+7 555 999 99 99', '6.jpg'),
+(7, 'john@north.no',         '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Jon',      'Snow',      '1988-10-11', 1, '+7 444 999 99 99', '7.jpg'),
+(8, 'dayneris@targarien.ta', '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Daenerys', 'Targaryen', '1981-03-17', 2, '+7 333 999 99 99', '8.jpg'),
+(9, 'rob@north.no',          '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Rob',      'Stark',     '1988-10-13', 1, '+7 222 999 99 99', '9.jpg'),
+(10, 'tywin@lannister.la',    '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Tywin',    'Lannister', '1952-08-27', 1, '+7 111 999 99 99', '10.jpg'),
+(11, 'brienne@tarth.ta',      '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Brienne',  'Tarth',     '1967-02-19', 2, '+7 999 888 99 99', '11.jpg'),
+(12, 'lord@varys.va',         '$2a$04$eFytJDGtjbThXa80FyOOBuFdK2IwjyWefYkMpiBEFlpBwDH.5PM0K', 'Lord',     'Varys',     '1954-12-01', 1, '+7 999 777 99 99', '12.jpeg');
 
 INSERT INTO user_role (user_id, role_id)
-VALUES 
+VALUES
 (1, 1),
 (1, 2),
 (2, 1),
@@ -47,8 +46,8 @@ VALUES
 (null, 1, 10, false),
 (null, 1,  8, false);
 
-INSERT INTO messages (id, time, sender, receiver, message) 
-VALUES 
+INSERT INTO messages (id, time, sender, receiver, message)
+VALUES
 (null, '2018-07-09 10:11:12', 1, 2, 'Привет! Как дела?'),
 (null, '2018-07-10 10:11:12', 2, 1, 'Привет! Спасибо, хорошо=)'),
 (null, '2018-07-11 10:11:12', 3, 1, 'Привет! Отец недоволен тобой! Он хочет тебя казнить'),
